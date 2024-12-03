8f4096ae21114a19ebd7d4e10e77dbb2b530bba0
Jacob Larson <hockeybobber@comcast.net>
2024-12-02 23:52:28 -0600

    added template for game world

---
+++ src/components/GameWorld/GameWorld.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
+
+function GameWorld() {
+
+
+  return (
+    <div>
+      <p>hi</p>
+    </div>
+  );
+}
+
+export default GameWorld;
