13adf014fbf83de038932ca81e5c3627c30d2e79
Jacob Larson <hockeybobber@comcast.net>
2024-12-03 22:18:35 -0600

    changed the import name for the lake background

--- src/components/Battle/Battle.jsx
+++ src/components/Battle/Battle.jsx
@@ -15,7 +15,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import lakeBackground from "../../ExportBackgroundnomoveclound.webp";
+import lakeBackground from "../../LakeBackground.webp";
 import forestBackground from "../../RockForest.webp";
 import ListItemText from "@mui/material/ListItemText";
 import ListItem from "@mui/material/ListItem";
