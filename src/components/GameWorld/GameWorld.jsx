88348a52ecca31e4746df8f4c2679a437f0c32af
Jacob Larson <hockeybobber@comcast.net>
2024-12-04 22:30:22 -0600

    got the canvas

--- src/components/GameWorld/GameWorld.jsx
+++ src/components/GameWorld/GameWorld.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
+import "./gameWorld.css";
 
 function GameWorld() {
 
+    const canvas = document.getElementById('canvasForGameWorld');
+
+    console.log('canvas', canvas);
+    
+
 
   return (
-    <canvas id='canvasForGameWorld'>
+    <canvas id='canvasForGameWorld' className='canvasForGame'>
 
     </canvas>
   );
