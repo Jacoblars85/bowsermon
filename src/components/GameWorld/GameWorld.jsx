126ab5f485bc4a07ba968877b79b0919ff713aee
Jacob Larson <hockeybobber@comcast.net>
2024-12-04 23:43:04 -0600

    the map now shows up on dom

--- src/components/GameWorld/GameWorld.jsx
+++ src/components/GameWorld/GameWorld.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef  } from 'react';
 import {useSelector} from 'react-redux';
 import "./gameWorld.css";
+import MarioMap from './bowsermon-map-v1.png'
 
 function GameWorld() {
     
@@ -11,12 +12,23 @@ function GameWorld() {
         const canvas = canvasRef.current;
         const c = canvas.getContext('2d'); 
   
-        console.log('c', c);
+        // console.log('c', c);
+
+        c.fillRect(0,0, canvas.width, canvas.height);
+        c.fillStyle = 'white';
+
+        const image = new Image()
+        image.src = MarioMap
+        // console.log(image);
+        
+        image.onload = () => {
+            c.drawImage(image, -4750, -5900)
+        }
       }
     }, []);
 
   return (
-    <canvas ref={canvasRef} id='canvasForGameWorld' className='canvasForGame'>
+    <canvas ref={canvasRef} height={576} width={1024} id='canvasForGameWorld' className='canvasForGame'>
 
     </canvas>
   );
