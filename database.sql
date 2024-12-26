0b4c53120aa28767735d238fd67e90280dd009ac
Jacob Larson <hockeybobber@comcast.net>
2024-12-26 00:39:13 -0600

    added a new consumable

--- database.sql
+++ database.sql
@@ -168,8 +168,9 @@ INSERT INTO "items"
 	('stamina pot', 'consumable', 0, 30, 0, 0, '', 10, '#00D400'),
 	('speed pot', 'consumable', 0, 50, 10, 0, '', 40, '#FF9A1E'),
 	('mega pot', 'consumable', 40, 40, 0, 0, '', 50, '#FFD42A'),
+	('beef', 'consumable', 30, 40, 0, 0, '', 50, '#FFD42A'),
 	('med kit', 'consumable', 75, 0, 0, 0, '', 70, '#FF0100'),
-	('golden pot', 'consumable', 100, 100, 0, 0, '', 120, '#FEF202'),
+	('heart', 'consumable', 100, 100, 0, 0, '', 120, '#FEF202'),
 
 	('fire', 'held', 10, 0, 0, 5, '', 100, '#000000'),
 	('shield', 'held', 20, 0, 0, 0, '', 150, '#000000'),
