e51f81e2a284faabb7c44a834f0ff577c03837fe
Jacob Larson <hockeybobber@comcast.net>
2024-12-31 16:15:59 -0600

    added basic attacks

--- database.sql
+++ database.sql
@@ -157,8 +157,16 @@ INSERT INTO "characters"
 INSERT INTO "attacks" 
 	("attack", "damage", "stamina", "attack_type", "hold_time", "max_frames", "fx_img")
 	VALUES 
-	('', 5, 5, 'physical', 0, 0, ''),
-	('', 1, 1, 'physical', 0, 0, '');
+	('fireball', 5, 5, 'projectile', 0, 0, ''),
+	('ember', 5, 5, 'projectile', 0, 0, ''),
+	('thunder', 5, 5, 'summon', 0, 0, ''),
+	('ice', 5, 5, 'summon', 0, 0, ''),
+	('', 5, 5, '', 0, 0, ''),
+	('', 5, 5, '', 0, 0, ''),
+	('', 5, 5, '', 0, 0, ''),
+	('strangle', 5, 5, 'physical', 0, 0, ''),
+	('life drain', 5, 5, 'physical', 0, 0, ''),
+	('bite', 1, 1, 'physical', 0, 0, '');
 	
 
 	
