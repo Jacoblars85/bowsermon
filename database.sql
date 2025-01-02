742d9c3ddb96ac1b346851e402de7b7be491f94d
Jacob Larson <hockeybobber@comcast.net>
2025-01-01 21:02:10 -0600

    added the animation id to table

--- database.sql
+++ database.sql
@@ -161,27 +161,27 @@ INSERT INTO "characters"
 
 
 INSERT INTO "attack_animations" 
-	("hold_time", "max_frames", "fx_img")
+	("attack_type", "hold_time", "max_frames", "fx_img")
 	VALUES 
-	(5, 5, ''),
-	(5, 5, ''),
-	(5, 5, ''),
-	(5, 5, ''),
-	(5, 5, ''),
-	(5, 5, ''),
-	(5, 5, ''),
-	(1, 1, '');
+	('', 5, 5, ''),
+	('', 5, 5, ''),
+	('', 5, 5, ''),
+	('', 5, 5, ''),
+	('', 5, 5, ''),
+	('', 5, 5, ''),
+	('', 5, 5, ''),
+	('', 1, 1, '');
 
 
 INSERT INTO "attacks" 
-	("attack", "damage", "stamina", "attack_type", "hold_time", "max_frames", "fx_img")
+	("attack", "damage", "stamina", "attack_animations_id")
 	VALUES 
-	('fireball', 5, 5, 'projectile', 0, 0, ''),
-	('ember', 5, 5, 'projectile', 0, 0, ''),
-	('thunder', 5, 5, 'summon', 0, 0, ''),
-	('ice', 5, 5, 'summon', 0, 0, ''),
-	('', 5, 5, '', 0, 0, ''),
-	('', 5, 5, '', 0, 0, ''),
+	('fireball', 5, 5, 'projectile', 0, 0, 0),
+	('ember', 5, 5, 'projectile', 0, 0, 0),
+	('thunder', 5, 5, 'summon', 0, 0, 0),
+	('ice', 5, 5, 'summon', 0, 0, 0),
+	('', 5, 5, '', 0, 0, 0),
+	('', 5, 5, '', 0, 0, 0),
 	('', 5, 5, '', 0, 0, ''),
 	('strangle', 5, 5, 'physical', 0, 0, ''),
 	('life drain', 5, 5, 'physical', 0, 0, ''),
