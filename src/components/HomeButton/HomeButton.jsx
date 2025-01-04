e04cd8627392b6323b120bb0ec627846b665cee9
Jacob Larson <hockeybobber@comcast.net>
2025-01-03 23:26:04 -0600

    formatted

--- src/components/HomeButton/HomeButton.jsx
+++ src/components/HomeButton/HomeButton.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function HomeButton() {
-    const history = useHistory()
-    const user = useSelector((store) => store.user.userReducer);
+  const history = useHistory();
+  const user = useSelector((store) => store.user.userReducer);
 
-    const goToHome = () => {
+  const goToHome = () => {
+    history.push(`/home`);
+  };
 
-        history.push(`/home`)
-    };
-
-    return (
-        <div>
-            <p onClick={goToHome}>Home</p>
-        </div>
-    );
+  return (
+    <div>
+      <p onClick={goToHome}>Home</p>
+    </div>
+  );
 }
 
 export default HomeButton;
