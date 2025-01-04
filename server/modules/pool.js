8f2d04968c780c960d4bd6eca27bb21b9ce7ba71
Jacob Larson <hockeybobber@comcast.net>
2025-01-03 22:44:15 -0600

    changed the database name

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,7 +28,7 @@ else {
     pool = new pg.Pool({
         host: 'localhost',
         port: 5432,
-        database: 'bowsermon',   // 	💥 Change this to the name of your database!
+        database: 'bowsermon_v2',   // 	💥 Change this to the name of your database!
     });
 }
 
