80e2d695c53d470df9bd9c92a45f419e4ec55ba5
Jacob Larson <hockeybobber@comcast.net>
2025-01-24 22:39:39 -0600

    formatted

--- src/components/UseCanvas/UseCanvas.jsx
+++ src/components/UseCanvas/UseCanvas.jsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useRef } from "react";
 
-const UseCanvas = draw => {
-
+const UseCanvas = (draw) => {
   const ref = useRef();
 
   useEffect(() => {
     // if (canvasRef.current) {
-      const canvas = ref.current;
-      const c = canvas.getContext("2d");
+    const canvas = ref.current;
+    const c = canvas.getContext("2d");
 
-      let animationId;
+    let animationId;
 
-      const renderer = () => {
-        draw(canvas, c)
-        animationId = window.requestAnimationFrame(renderer)
-      }
-      renderer()
+    const renderer = () => {
+      draw(canvas, c);
+      animationId = window.requestAnimationFrame(renderer);
+    };
+    renderer();
 
-      return () => window.cancelAnimationFrame(animationId)
+    return () => window.cancelAnimationFrame(animationId);
     // }
   }, [draw]);
 
-  return ref
-}
+  return ref;
+};
 
 export default UseCanvas;
