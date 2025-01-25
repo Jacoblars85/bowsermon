84fa35463f422960683ef338c35edeac7a581b70
Jacob Larson <hockeybobber@comcast.net>
2025-01-24 22:42:45 -0600

    deleted and finished the components

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -4,8 +4,6 @@ import UseCanvas from "../UseCanvas/UseCanvas";
 function Canvas(props) {
   const { draw, ...rest } = props;
 
-  //   console.log(rest);
-
   const ref = UseCanvas(draw);
 
   return <canvas ref={ref} {...rest} />;
