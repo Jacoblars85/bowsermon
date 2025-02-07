b3920601d7674fa5bba3f89a4105a238e6dc8bae
Jacob Larson <hockeybobber@comcast.net>
2025-02-06 23:07:00 -0600

    replaced the old cprite class

--- src/components/GameWorld/GameWorld.jsx
+++ src/components/GameWorld/GameWorld.jsx
@@ -113,13 +113,57 @@ function GameWorld() {
       const playerRightImage = new Image();
       playerRightImage.src = PlayerRight;
 
+      // class Sprite {
+      //   constructor({
+      //     position,
+      //     image,
+      //     frames = { max: 1, hold: 10 },
+      //     sprites,
+      //     animate = false,
+      //   }) {
+      //     this.position = position;
+      //     this.image = image;
+      //     (this.frames = { ...frames, val: 0, elapsed: 0 }),
+      //       (this.image.onload = () => {
+      //         this.width = this.image.width / this.frames.max;
+      //         this.height = this.image.height;
+      //       });
+      //     this.animate = animate;
+      //     this.sprites = sprites;
+      //   }
+
+      //   draw() {
+      //     c.drawImage(
+      //       this.image,
+      //       this.frames.val * this.width,
+      //       0,
+      //       this.image.width / this.frames.max,
+      //       this.image.height,
+      //       this.position.x,
+      //       this.position.y,
+      //       this.image.width / this.frames.max,
+      //       this.image.height
+      //     );
+      //     if (!this.animate) return;
+      //     if (this.frames.max > 1) {
+      //       this.frames.elapsed++;
+      //     }
+      //     if (this.frames.elapsed % this.frames.hold === 0) {
+      //       if (this.frames.val < this.frames.max - 1) this.frames.val++;
+      //       else this.frames.val = 0;
+      //     }
+      //   }
+      // }
+
       class Sprite {
         constructor({
           position,
           image,
-          frames = { max: 1, hold: 10 },
+          frames = { max: 1, hold: 10, alignment: 0, attackFx: false },
           sprites,
           animate = false,
+          rotation = 0,
+          opacity = 1,
         }) {
           this.position = position;
           this.image = image;
@@ -130,20 +174,74 @@ function GameWorld() {
             });
           this.animate = animate;
           this.sprites = sprites;
+          this.opacity = opacity;
+          this.rotation = rotation;
         }
 
         draw() {
-          c.drawImage(
-            this.image,
-            this.frames.val * this.width,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
+          c.save();
+          c.translate(
+            this.position.x + this.width / 2,
+            this.position.y + this.height / 2
           );
+          c.rotate(this.rotation);
+          c.translate(
+            -this.position.x - this.width / 2,
+            -this.position.y - this.height / 2
+          );
+          c.globalAlpha = this.opacity;
+
+          if (this.frames.attackFx) {
+            c.drawImage(
+              this.image,
+              this.frames.val * this.width,
+              0,
+              this.image.width / this.frames.max,
+              this.image.height,
+              this.position.x,
+              this.position.y,
+              this.image.width / this.frames.max,
+              this.image.height
+            );
+          } else {
+            c.drawImage(
+              this.image,
+              this.frames.alignment,
+              this.frames.val * this.width,
+              this.image.width / this.frames.max,
+              this.image.height / this.frames.max,
+              this.position.x,
+              this.position.y,
+              this.image.width / this.frames.max,
+              this.image.height / this.frames.max
+            );
+          }
+
+          // c.drawImage(
+          //   this.image,
+          //   this.frames.val * this.width,
+          //   0,
+          //   this.image.width / this.frames.max,
+          //   this.image.height,
+          //   this.position.x,
+          //   this.position.y,
+          //   this.image.width / this.frames.max,
+          //   this.image.height
+          // );
+
+          // c.drawImage(
+          //   this.image,
+          //   this.frames.alignment,
+          //   this.frames.val * this.width,
+          //   this.image.width / this.frames.max,
+          //   this.image.height / this.frames.max,
+          //   this.position.x,
+          //   this.position.y,
+          //   this.image.width / this.frames.max,
+          //   this.image.height / this.frames.max
+          // );
+          c.restore();
+
           if (!this.animate) return;
           if (this.frames.max > 1) {
             this.frames.elapsed++;
@@ -164,6 +262,7 @@ function GameWorld() {
         frames: {
           max: 4,
           hold: 10,
+          attackFx: true,
         },
         sprites: {
           up: playerUpImage,
@@ -181,7 +280,7 @@ function GameWorld() {
         image: foregroundImage,
       });
 
-      const background = new Sprite({
+      const exploringBackground = new Sprite({
         position: {
           x: offset.x,
           y: offset.y,
@@ -204,7 +303,7 @@ function GameWorld() {
         },
       };
 
-      const movables = [background, ...boundaries, foreground, ...battleZones];
+      const movables = [exploringBackground, ...boundaries, foreground, ...battleZones];
 
       function rectangularCollisions({ rectangle1, rectangle2 }) {
         return (
@@ -444,103 +543,103 @@ function GameWorld() {
       const starterTwoImage = new Image();
       starterTwoImage.src = starterTwoPicture;
 
-      class Sprite {
-        constructor({
-          position,
-          image,
-          frames = { max: 1, hold: 10, alignment: 0, attackFx: false },
-          sprites,
-          animate = false,
-          rotation = 0,
-          opacity = 1,
-        }) {
-          this.position = position;
-          this.image = image;
-          (this.frames = { ...frames, val: 0, elapsed: 0 }),
-            (this.image.onload = () => {
-              this.width = this.image.width / this.frames.max;
-              this.height = this.image.height;
-            });
-          this.animate = animate;
-          this.sprites = sprites;
-          this.opacity = opacity;
-          this.rotation = rotation;
-        }
+      // class Sprite {
+      //   constructor({
+      //     position,
+      //     image,
+      //     frames = { max: 1, hold: 10, alignment: 0, attackFx: false },
+      //     sprites,
+      //     animate = false,
+      //     rotation = 0,
+      //     opacity = 1,
+      //   }) {
+      //     this.position = position;
+      //     this.image = image;
+      //     (this.frames = { ...frames, val: 0, elapsed: 0 }),
+      //       (this.image.onload = () => {
+      //         this.width = this.image.width / this.frames.max;
+      //         this.height = this.image.height;
+      //       });
+      //     this.animate = animate;
+      //     this.sprites = sprites;
+      //     this.opacity = opacity;
+      //     this.rotation = rotation;
+      //   }
 
-        draw() {
-          c.save();
-          c.translate(
-            this.position.x + this.width / 2,
-            this.position.y + this.height / 2
-          );
-          c.rotate(this.rotation);
-          c.translate(
-            -this.position.x - this.width / 2,
-            -this.position.y - this.height / 2
-          );
-          c.globalAlpha = this.opacity;
+      //   draw() {
+      //     c.save();
+      //     c.translate(
+      //       this.position.x + this.width / 2,
+      //       this.position.y + this.height / 2
+      //     );
+      //     c.rotate(this.rotation);
+      //     c.translate(
+      //       -this.position.x - this.width / 2,
+      //       -this.position.y - this.height / 2
+      //     );
+      //     c.globalAlpha = this.opacity;
 
-          if (this.frames.attackFx) {
-            c.drawImage(
-              this.image,
-              this.frames.val * this.width,
-              0,
-              this.image.width / this.frames.max,
-              this.image.height,
-              this.position.x,
-              this.position.y,
-              this.image.width / this.frames.max,
-              this.image.height
-            );
-          } else {
-            c.drawImage(
-              this.image,
-              this.frames.alignment,
-              this.frames.val * this.width,
-              this.image.width / this.frames.max,
-              this.image.height / this.frames.max,
-              this.position.x,
-              this.position.y,
-              this.image.width / this.frames.max,
-              this.image.height / this.frames.max
-            );
-          }
+      //     if (this.frames.attackFx) {
+      //       c.drawImage(
+      //         this.image,
+      //         this.frames.val * this.width,
+      //         0,
+      //         this.image.width / this.frames.max,
+      //         this.image.height,
+      //         this.position.x,
+      //         this.position.y,
+      //         this.image.width / this.frames.max,
+      //         this.image.height
+      //       );
+      //     } else {
+      //       c.drawImage(
+      //         this.image,
+      //         this.frames.alignment,
+      //         this.frames.val * this.width,
+      //         this.image.width / this.frames.max,
+      //         this.image.height / this.frames.max,
+      //         this.position.x,
+      //         this.position.y,
+      //         this.image.width / this.frames.max,
+      //         this.image.height / this.frames.max
+      //       );
+      //     }
 
-          // c.drawImage(
-          //   this.image,
-          //   this.frames.val * this.width,
-          //   0,
-          //   this.image.width / this.frames.max,
-          //   this.image.height,
-          //   this.position.x,
-          //   this.position.y,
-          //   this.image.width / this.frames.max,
-          //   this.image.height
-          // );
+      //     // c.drawImage(
+      //     //   this.image,
+      //     //   this.frames.val * this.width,
+      //     //   0,
+      //     //   this.image.width / this.frames.max,
+      //     //   this.image.height,
+      //     //   this.position.x,
+      //     //   this.position.y,
+      //     //   this.image.width / this.frames.max,
+      //     //   this.image.height
+      //     // );
 
-          // c.drawImage(
-          //   this.image,
-          //   this.frames.alignment,
-          //   this.frames.val * this.width,
-          //   this.image.width / this.frames.max,
-          //   this.image.height / this.frames.max,
-          //   this.position.x,
-          //   this.position.y,
-          //   this.image.width / this.frames.max,
-          //   this.image.height / this.frames.max
-          // );
-          c.restore();
+      //     // c.drawImage(
+      //     //   this.image,
+      //     //   this.frames.alignment,
+      //     //   this.frames.val * this.width,
+      //     //   this.image.width / this.frames.max,
+      //     //   this.image.height / this.frames.max,
+      //     //   this.position.x,
+      //     //   this.position.y,
+      //     //   this.image.width / this.frames.max,
+      //     //   this.image.height / this.frames.max
+      //     // );
+      //     c.restore();
 
-          if (!this.animate) return;
-          if (this.frames.max > 1) {
-            this.frames.elapsed++;
-          }
-          if (this.frames.elapsed % this.frames.hold === 0) {
-            if (this.frames.val < this.frames.max - 1) this.frames.val++;
-            else this.frames.val = 0;
-          }
-        }
-      }
+      //     if (!this.animate) return;
+      //     if (this.frames.max > 1) {
+      //       this.frames.elapsed++;
+      //     }
+      //     if (this.frames.elapsed % this.frames.hold === 0) {
+      //       if (this.frames.val < this.frames.max - 1) this.frames.val++;
+      //       else this.frames.val = 0;
+      //     }
+      //   }
+      // }
 
       class Character extends Sprite {
         constructor({
