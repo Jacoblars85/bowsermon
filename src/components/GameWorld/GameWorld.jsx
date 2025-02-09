f83264a01b912ef527652ccbae792f20de45f620
Jacob Larson <hockeybobber@comcast.net>
2025-02-08 22:56:34 -0600

    deleted another old sprite class

--- src/components/GameWorld/GameWorld.jsx
+++ src/components/GameWorld/GameWorld.jsx
@@ -353,48 +353,6 @@ function GameWorld() {
       const playerRightImage = new Image();
       playerRightImage.src = PlayerRight;
 
-      // class Sprite {
-      //   constructor({
-      //     position,
-      //     image,
-      //     frames = { max: 1, hold: 10 },
-      //     sprites,
-      //     animate = false,
-      //   }) {
-      //     this.position = position;
-      //     this.image = image;
-      //     (this.frames = { ...frames, val: 0, elapsed: 0 }),
-      //       (this.image.onload = () => {
-      //         this.width = this.image.width / this.frames.max;
-      //         this.height = this.image.height;
-      //       });
-      //     this.animate = animate;
-      //     this.sprites = sprites;
-      //   }
-
-      //   draw() {
-      //     c.drawImage(
-      //       this.image,
-      //       this.frames.val * this.width,
-      //       0,
-      //       this.image.width / this.frames.max,
-      //       this.image.height,
-      //       this.position.x,
-      //       this.position.y,
-      //       this.image.width / this.frames.max,
-      //       this.image.height
-      //     );
-      //     if (!this.animate) return;
-      //     if (this.frames.max > 1) {
-      //       this.frames.elapsed++;
-      //     }
-      //     if (this.frames.elapsed % this.frames.hold === 0) {
-      //       if (this.frames.val < this.frames.max - 1) this.frames.val++;
-      //       else this.frames.val = 0;
-      //     }
-      //   }
-      // }
-
       class Sprite {
         constructor({
           position,
